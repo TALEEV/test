e72ea67a065b185911040e076a6bd2d31c9b3a79
TALEEV <alammdtalib@gmail.com>
2020-06-17 15:52:16 +0530

    Commiting the first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <FormComponent />
+  </div>
+</template>
+
+<script>
+import FormComponent from './components/FormComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormComponent
+  }
+}
+</script>
